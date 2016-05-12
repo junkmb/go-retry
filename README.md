3de907b74bc7c126fa96026e0ebf8c6a57316050
Jun Kambe <kambe-jun@iij.ad.jp>
2016-05-12 13:35:41 +0900

    Add DurationRetryer

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
+example
 ```
 var i int
-r := NewRetryer(time.Second, 5)
+r := NewCountRetryer(time.Second, 5)
 for r.Loop() {
 	fmt.Println(i)
 	i++
--- retry.go
+++ retry.go
@@ -5,25 +5,25 @@ import (
 	"time"
 )
 
-type Retryer struct {
-	maxTry   int
+type CountRetryer struct {
+	maxCount int
 	interval time.Duration
 	count    int
 	started  bool
 	m        sync.Mutex
 }
 
-func NewRetryer(maxTry int, interval time.Duration) *Retryer {
-	return &Retryer{
-		maxTry:   maxTry,
+func NewCountRetryer(maxCount int, interval time.Duration) *CountRetryer {
+	return &CountRetryer{
+		maxCount: maxCount,
 		interval: interval,
 	}
 }
 
-func (r *Retryer) Loop() bool {
+func (r *CountRetryer) Loop() bool {
 	r.m.Lock()
 	defer r.m.Unlock()
-	if r.count == r.maxTry {
+	if r.count == r.maxCount {
 		return false
 	}
 	r.count++
@@ -32,5 +32,35 @@ func (r *Retryer) Loop() bool {
 	} else {
 		r.started = true
 	}
+	return true
+}
+
+type DurationRetryer struct {
+	limit    time.Time
+	interval time.Duration
+	finished bool
+	started  bool
+	m        sync.Mutex
+}
+
+func NewDurationRetryer(maxDuration, interval time.Duration) *DurationRetryer {
+	return &DurationRetryer{
+		limit:    time.Now().Add(maxDuration),
+		interval: interval,
+	}
+}
+
+func (r *DurationRetryer) Loop() bool {
+	r.m.Lock()
+	defer r.m.Unlock()
+	if r.finished {
+		return false
+	}
+	if r.started {
+		time.Sleep(r.interval)
+	} else {
+		r.started = true
+	}
+	r.finished = time.Now().Add(r.interval).After(r.limit)
 	return true
 }
